2ca84892457239095a521e375b0fb40bcc0abfde
Viniski <799048283mw@gmail.com>
2023-02-23 20:44:13 +0100

    Add a few unit tests

---
+++ src/component/Header/Header.test.js
@@ -0,0 +1,19 @@
+import { render, screen } from "@testing-library/react";
+import React from "react";
+import App from "../../App";
+import { Provider } from "react-redux";
+import store from "../../redux/store";
+
+describe("Header name", () => {
+  const expectedTitle = "Strona główna";
+
+  it("should render the header name given in title props", () => {
+    render(
+      <Provider store={store}>
+        <App />
+      </Provider>
+    );
+    const header = screen.getByRole("heading", { name:  "Strona główna"});
+    expect(header).toBeInTheDocument();
+  });
+});
