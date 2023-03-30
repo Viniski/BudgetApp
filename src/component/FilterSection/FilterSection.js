1dfa621c9adecabdfd1708e15d89871c078d29e8
Viniski <799048283mw@gmail.com>
2023-03-30 11:07:29 +0200

    Create hooks to filterSection and TransactionSection components

--- src/component/FilterSection/FilterSection.js
+++ src/component/FilterSection/FilterSection.js
@@ -1,188 +1,29 @@
-import { useState } from "react";
-import { useDispatch } from "react-redux";
-import {
-  updateHomeURL,
-  updateExpenseURL,
-  updateIncomeURL,
-} from "../../redux/urlSlice";
 import { Input } from "../../UI/Inputs/Input";
 import { CheckboxCategoryInput } from "../../UI/Inputs/CheckboxCategoryInput";
 import { ActiveFilterCriteria } from "./ActiveFilterCriteria";
-import { formatDate } from "../../helpers/formatDate";
 import { FilterButton } from "../../UI/Buttons/FilterButton";
 import { CloseFilterButton } from "../../UI/Buttons/CloseFilterButton";
-import { createFilterUrl } from "../../helpers/createFilterUrl";
-import { useNavigate } from "react-router-dom";
-import { useLocation } from "react-router-dom";
+import { useFilterSection } from "./useFilterSection";
 
 export function FilterSection({ type, title, criteria, isDarkTheme }) {
-  const navigate = useNavigate();
-  const location = useLocation();
-  const dispatch = useDispatch();
-  const [minAmount, setMinAmount] = useState("");
-  const [maxAmount, setMaxAmount] = useState("");
-  const [startDate, setStartDate] = useState("");
-  const today = formatDate(new Date());
-  const [endDate, setEndDate] = useState("");
-
-  const getSelectedCategory = () => {
-    if (location.pathname === "/") {
-      return [
-        "Dochód stały",
-        "Dochód dodatkowy",
-        "Koszty stałe",
-        "Jedzenie",
-        "Transport",
-        "Rozrywka",
-        "Inne",
-      ];
-    }
-    if (location.pathname === "/wydatki") {
-      return ["Koszty stałe", "Jedzenie", "Transport", "Rozrywka", "Inne"];
-    }
-
-    return ["Dochód stały", "Dochód dodatkowy", "Inne"];
-  };
-
-  const [selectedCategory, setSelectedCategory] = useState(
-    getSelectedCategory()
-  );
-
-  const [filterSectionState, setFilterSectionState] = useState({
-    isFormActive: false,
-    activeCriteria: {
-      minAmount,
-      maxAmount,
-      startDate,
-      endDate,
-      selectedCategory,
-    },
-  });
-
-  const getCategoryToDelete = (selectedCategory) => {
-    let arrayAllCategories = getSelectedCategory();
-
-    for (let i = 0; i < selectedCategory.length; i++) {
-      arrayAllCategories = arrayAllCategories.filter(
-        (element) => element !== selectedCategory[i]
-      );
-    }
-
-    return [...arrayAllCategories];
-  };
-
-  const objectToFilter = {
+  const {
     minAmount,
+    setMinAmount,
     maxAmount,
+    setMaxAmount,
+    today,
     startDate,
+    setStartDate,
     endDate,
-    selectedCategory: getCategoryToDelete(selectedCategory),
-  };
-
-  const toogleFilterButton = () => {
-    setFilterSectionState({
-      ...filterSectionState,
-      isFormActive: !filterSectionState.isFormActive,
-    });
-  };
-
-  const changeCheckboxInput = (e) => {
-    const value = e.target.value;
-    const isChecked = e.target.checked;
-
-    if (isChecked) {
-      const newSelectedCategory = [...selectedCategory, value];
-      setSelectedCategory(newSelectedCategory);
-    } else {
-      const newSelectedCategory = selectedCategory.filter(
-        (category) => category !== value
-      );
-      setSelectedCategory(newSelectedCategory);
-    }
-  };
-
-  const handleFilterButton = (params) => {
-    const newUrl = createFilterUrl(location.pathname, params);
-    setFilterSectionState({ activeCriteria: params, isFormActive: false });
-    dispatch(
-      location.pathname === "/"
-        ? updateHomeURL(newUrl)
-        : location.pathname === "/wydatki"
-        ? updateExpenseURL(newUrl)
-        : updateIncomeURL(newUrl)
-    );
-    navigate(newUrl);
-  };
-
-  const handleDeleteCriteria = (params) => {
-    const newUrl = createFilterUrl(location.pathname, params);
-    setFilterSectionState({ activeCriteria: params, isFormActive: false }); //why?
-    dispatch(
-      location.pathname === "/"
-        ? updateHomeURL(newUrl)
-        : location.pathname === "/wydatki"
-        ? updateExpenseURL(newUrl)
-        : updateIncomeURL(newUrl)
-    );
-    navigate(newUrl);
-  };
-
-  const deleteFilterCriteria = (criteriaToDelete) => {
-    switch (criteriaToDelete) {
-      case "minAmount":
-        handleDeleteCriteria({
-          minAmount: "",
-          maxAmount,
-          startDate,
-          endDate,
-          selectedCategory: getCategoryToDelete(selectedCategory),
-        });
-        setMinAmount("");
-        break;
-      case "maxAmount":
-        handleDeleteCriteria({
-          minAmount,
-          maxAmount: "",
-          startDate,
-          endDate,
-          selectedCategory: getCategoryToDelete(selectedCategory),
-        });
-        setMaxAmount("");
-        break;
-      case "startDate":
-        handleDeleteCriteria({
-          minAmount,
-          maxAmount,
-          startDate: "",
-          endDate,
-          selectedCategory: getCategoryToDelete(selectedCategory),
-        });
-        setStartDate("");
-        break;
-      case "endDate":
-        handleDeleteCriteria({
-          minAmount,
-          maxAmount,
-          startDate,
-          endDate: "",
-          selectedCategory: getCategoryToDelete(selectedCategory),
-        });
-        setEndDate("");
-        break;
-      case "selectedCategory":
-        handleDeleteCriteria({
-          minAmount,
-          maxAmount,
-          startDate,
-          endDate,
-          selectedCategory: getCategoryToDelete(getSelectedCategory()),
-        });
-        setSelectedCategory(getSelectedCategory());
-        break;
-      default:
-        break;
-    }
-  };
+    setEndDate,
+    selectedCategory,
+    changeCheckboxInput,
+    handleFilterButton,
+    filterSectionState,
+    toogleFilterButton,
+    objectToFilter,
+    deleteFilterCriteria,
+  } = useFilterSection();
 
   return (
     <>
--- src/component/TransactionsSection/TransactionsSection.js
+++ src/component/TransactionsSection/TransactionsSection.js
@@ -1,84 +1,17 @@
-import { useNavigate, useSearchParams } from "react-router-dom";
-import { useSelector, useDispatch } from "react-redux";
-import {
-  updateHomeURL,
-  updateExpenseURL,
-  updateIncomeURL,
-} from "../../redux/urlSlice";
 import { TransactionCard } from "../TransactionCard/TransactionCard";
 import { FilterSection } from "../FilterSection/FilterSection";
 import { Pagination } from "../../component/Pagination/Pagination";
-import { getFilteredTransaction } from "../../helpers/getFilteredTransaction";
-import { useLocation } from "react-router-dom";
-import { createPaginationUrl } from "../../helpers/createPaginationUrl";
+import { useTransactionSection } from "./useTransactionSection";
 
 export function TransactionSection({ type, title }) {
-  const navigate = useNavigate();
-  const location = useLocation();
-  const dispatch = useDispatch();
-
-  const isDarkTheme = useSelector((state) => state.theme.isDarkTheme);
-
-  const [searchParams] = useSearchParams();
-
-  const getParamsToFilterFromURL = () => {
-    const objectParams = {
-      page: searchParams.get("strona"),
-      minAmount: searchParams.get("min"),
-      maxAmount: searchParams.get("max"),
-      endDate: searchParams.get("do"),
-      startDate: searchParams.get("od"),
-      selectedCategory: searchParams.get("delete_category")?.split(",") || [],
-    };
-
-    return objectParams;
-  };
-
-  let stateTransactions = useSelector((state) => state.transactions);
-  const getTransactions = () => {
-    if (type === "expense") {
-      stateTransactions = stateTransactions.filter(
-        (el) => el.type === "expense"
-      );
-    }
-
-    if (type === "income") {
-      stateTransactions = stateTransactions.filter(
-        (el) => el.type === "income"
-      );
-    }
-
-    return stateTransactions;
-  };
-
-  const stateTransaction = getTransactions();
-
-  const transactions = getFilteredTransaction(
-    stateTransaction,
-    getParamsToFilterFromURL()
-  );
-
-  const currentPage = searchParams.get("strona") || 1;
-  const perPage = 5;
-  const startIndex = (currentPage - 1) * perPage;
-  const endIndex = currentPage * perPage - 1;
-  let transactionOnPage = transactions.slice(startIndex, endIndex + 1);
-
-  const paginate = (number) => {
-    const newUrl = createPaginationUrl(
-      location.pathname,
-      getParamsToFilterFromURL(),
-      number
-    );
-    dispatch(
-      location.pathname === "/"
-        ? updateHomeURL(newUrl)
-        : type === "/wydatki"
-        ? updateExpenseURL(newUrl)
-        : updateIncomeURL(newUrl)
-    );
-    navigate(newUrl);
-  };
+  const {
+    isDarkTheme,
+    transactions,
+    perPage,
+    transactionOnPage,
+    criteria,
+    paginate,
+  } = useTransactionSection(type);
 
   return (
     <section className="transaction-section">
@@ -86,7 +19,7 @@ export function TransactionSection({ type, title }) {
         type={type}
         title={title}
         isDarkTheme={isDarkTheme}
-        criteria={getParamsToFilterFromURL()}
+        criteria={criteria}
       />
       {transactionOnPage.map((transaction) => (
         <TransactionCard
